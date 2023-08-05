38a0708285472881f7be8372c9efe53f0f9403e0
soumen bhunia <sob99338@gmail.com>
2023-08-05 11:41:51 +0530

    making this on react

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    
+    <section class="sec-1">
+        <nav>
+            <div class="navElements">
+                <div class="logo"> <span class="logo_icon">W</span>WorkFolio</div>
+                <div class="LinkComponents">
+                    <ul>
+                        <li>Templates</li>
+                        <li>Examples</li>
+                        <li>Services</li>
+                        <li>About</li>
+                        <li>Help</li>
+                    </ul>
+                </div>
+
+                <div class="user_click_buttons">
+                    <div class="logInButton"> Login</div>
+                    <div class="Create_cv">Create Resume</div>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+</>
+  );
+}
+
+export default App;
