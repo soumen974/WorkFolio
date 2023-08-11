c83617bec49474c1d8d9cd5991c4a05f1030694a
soumen bhunia <sob99338@gmail.com>
2023-08-11 23:57:05 +0530

    every thing gooing great

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from 'react';
 
 import NavBar from "./Components/NavBar";
 import Section01 from './Components/Section01';
+import Section02 from './Components/Section02';
 
 function App() {
   return (
     <>
         <NavBar/>
         <Section01/>
+        {/* <Section02/> */}
     </>
   );
 }
---
+++ src/Components/Section02.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Section02.css'
+
+export default function Section02() {
+  return (
+    <>
+    working  on for section 2
+    </>
+  )
+}
