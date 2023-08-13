0e55de046f826395f84fed17b0f73eee490aaad1
soumen bhunia <sob99338@gmail.com>
2023-08-13 15:55:50 +0530

    going on to the Resume making form :

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React, { useState, useEffect } from 'react';
 import NavBar from "./Components/NavBar";
 import Section01 from './Components/Section01';
 import Section02 from './Components/Section02';
+import FormResume from './Components/FormResume';
 
 function App() {
   return (
     <>
         <NavBar/>
-        <Section01/>
-        <Section02/>
+        {/* <Section01/> */}
+        <FormResume/>
+        {/* <Section02/> */}
     </>
   );
 }
---
+++ src/Components/FormResume.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './FormResume.css'
+
+export default function FormResume() {
+  return (
+    <>
+      <div className="FormBody">
+
+        <div className="FormHeadder">
+            <h1>Give your Details</h1>
+                <h3>Trust me, I am not ZUK.</h3>
+        </div>
+
+
+        <div className="Form-01">
+            <div className="Uplodeimg">
+                <svg></svg>
+                <h6>Upload your Image </h6>
+            </div>
+
+        </div>
+
+        <div className="Form-02"></div>
+
+      </div>
+    </>
+  )
+}
