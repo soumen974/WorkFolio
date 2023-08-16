7a840f9b1204ab86c14e375d8c4be5b92d3d570a
soumen bhunia <sob99338@gmail.com>
2023-08-16 22:10:59 +0530

    workin on the secion 02, for the structure the things

--- src/Components/Section02.jsx
+++ src/Components/Section02.jsx
@@ -4,6 +4,18 @@ import './Section02.css'
 export default function Section02() {
   return (
     <>
+    <div className="sec-02-whole-body">
+
+      <div className="headder-sec-02">
+        <div className="circle-vector"></div>
+        <h1></h1>
+        <h4></h4>
+      </div>
+      
+      <div className="blockOfSqures"></div>
+      <div className="Revews-boxs"></div>
+
+    </div>
     
     </>
   )
