8e1c311d347478bc1e59ff16d251e1ff472c3ee8
soumen bhunia <sob99338@gmail.com>
2023-08-17 21:47:31 +0530

    addign the structure in the section 02

--- src/Components/Section02.jsx
+++ src/Components/Section02.jsx
@@ -8,12 +8,58 @@ export default function Section02() {
 
       <div className="headder-sec-02">
         <div className="circle-vector"></div>
-        <h1></h1>
-        <h4></h4>
+        <h1>What people say about us</h1>
+        <h4>Empowering Career Success: Real Stories from Our Users</h4>
       </div>
       
       <div className="blockOfSqures"></div>
-      <div className="Revews-boxs"></div>
+
+      <div className="Reviews-boxs">
+
+        <div className="Review-box-model">
+          <div className="head-box-model">
+            <div className="review-er-details">
+              <div className="review-er-image"></div>
+              <div className="review-er-name-post">
+                <h1>Soumen Bhunia</h1>
+                <h6>CEO</h6>
+              </div>
+            </div>
+            <div className="review-er-rating"></div>
+          </div>
+          <div className="riview-text">" As someone who spent a long time unemployed in the past , I absolutely love this. Well done guys! I really hope this gets wildly popular, it will be valuable to so many people. "</div>
+        </div>
+
+        <div className="Review-box-model">
+          <div className="head-box-model">
+            <div className="review-er-details">
+              <div className="review-er-image"></div>
+              <div className="review-er-name-post">
+                <h1>Soumen Bhunia</h1>
+                <h6>CEO</h6>
+              </div>
+            </div>
+            <div className="review-er-rating"></div>
+          </div>
+          <div className="riview-text">" As someone who spent a long time unemployed in the past , I absolutely love this. Well done guys! I really hope this gets wildly popular, it will be valuable to so many people. "</div>
+        </div>
+
+        <div className="Review-box-model">
+          <div className="head-box-model">
+            <div className="review-er-details">
+              <div className="review-er-image"></div>
+              <div className="review-er-name-post">
+                <h1>Soumen Bhunia</h1>
+                <h6>CEO</h6>
+              </div>
+            </div>
+            <div className="review-er-rating"></div>
+          </div>
+          <div className="riview-text">" As someone who spent a long time unemployed in the past , I absolutely love this. Well done guys! I really hope this gets wildly popular, it will be valuable to so many people. "</div>
+        </div>
+
+     
+      </div>
 
     </div>
     
