b345de3835a1cc53638b8a43e00af9d7a8c93fe4
soumen bhunia <sob99338@gmail.com>
2023-09-11 21:33:18 +0530

    footer done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -11,7 +11,7 @@ export default function NavBar(props) {
     <section className="sec-1">
         <nav>
             <div className="navElements">
-               <a href='#'> <div className="logo"  onClick={props.GotoHome}> <span className="logo_icon"><abbr title="WorkFolio Home">W</abbr></span><h3>WorkFolio</h3></div></a>
+               <a href='#'> <div className="logo"  onClick={props.GotoHome}> <span className="logo_icon"><abbr title="WorkFolio Home">W</abbr></span><h3>WorkFolios</h3></div></a>
                 <div className="LinkComponents">
                     <ul>
                      <a href='#'> <li onClick={props.GotoHome}>Home</li></a>
