5afd422670f5bd4cb62a45acf87a725b0b5b0b94
soumen bhunia <sob99338@gmail.com>
2023-09-22 22:30:12 +0530

    Resume styling & data outsourcing updated

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -26,37 +26,32 @@ export default function Resume({UserFirstName,
   return (
     <>
 
-<br />
-<br />
-<br />
-<br />
-<br />
+
 
     <div className="ResumeWhole-body">
 
       <div className="resumeWholeBack">
       <div className="ResumeHead">
+
             <div className="imageleft">
-            {selectedImage && (
-        <img src={URL.createObjectURL(selectedImage)} alt="Selected" />
-      )}
+                  {selectedImage && (
+                      <img src={URL.createObjectURL(selectedImage)} alt="Selected" />
+                  )}
 
+                  <div className="Name-post">
+                      <h1> {UserFirstName}  </h1>
+                      <h1> {UserLastName}</h1>
+                  </div>
+                  <h4>{UserWorkPost}</h4>
+             </div>
 
-            </div>
-            <div className="Name-post">
-            <h1> {UserFirstName}  </h1>
-            <h1> {UserLastName}</h1>
-            <h4>{UserWorkPost}</h4>
-
-              </div>
-          </div>
+        </div>
       <div className="ResumeDetails">
-        <div className="leftBlackDiv"></div>
         <div className="rightWhiteDiv">
 
 
 
-        <br/>
+        
                                   {UserSkills}<br/>
                                   {UserProjects}<br/>
                                   {UserEducation}<br/>
