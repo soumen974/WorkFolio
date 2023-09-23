8a6d718dbdbd355d0d584cf4e67b5ef648488fa9
soumen bhunia <sob99338@gmail.com>
2023-09-24 00:39:51 +0530

    making the resume download-able

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Resume.css'
+import {useState} from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export default function Resume({UserFirstName,
                                 UserLastName,
@@ -23,7 +26,26 @@ export default function Resume({UserFirstName,
 
                               
                               },props) {
+
+
+                                const [loader, setLoader] = useState(false);
+
+                                const downloadPDF = () =>{
+                                  const capture = document.querySelector('.resumeWholeBack');
+                                  setLoader(true);
+                                  html2canvas(capture).then((canvas)=>{
+                                    const imgData = canvas.toDataURL('img/png');
+                                    const doc = new jsPDF('p', 'mm', 'a4');
+                                    const componentWidth = doc.internal.pageSize.getWidth();
+                                    const componentHeight = doc.internal.pageSize.getHeight();
+                                    doc.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight);
+                                    setLoader(false);
+                                    doc.save('Resume-from-workfolios-by-soumen-bhunia.pdf');
+                                  })
+                                }
+
   return (
+    
     <>
 
 
@@ -78,6 +100,24 @@ export default function Resume({UserFirstName,
      
                                  
      </div>
+
+      <div className="receipt-actions-div">
+            <div className="actions-right">
+              <button
+                className="receipt-modal-download-button"
+                onClick={downloadPDF}
+                disabled={!(loader===false)}
+              >
+                {loader?(
+                  <span>Downloading</span>
+                ):(
+                  <span>Download</span>
+                )}
+
+              </button> 
+            </div>
+          </div>
+
     
     </div>
     
