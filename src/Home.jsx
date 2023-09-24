c062bdc1fa115ae4be544d899472e09e7ba246b9
soumen bhunia <sob99338@gmail.com>
2023-09-24 18:52:57 +0530

    adding new favicon & title

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,8 @@ import Section02 from './Components/Section02'
 import Section03 from './Components/Section03'
 
 export default function Home() {
+  document.title = 'WorkFolios ';
+
   return (
     <>
     <Section01/>
