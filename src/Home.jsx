7c33a29d36ec4d7651ffe57cb3aea51a957138a3
soumen bhunia <sob99338@gmail.com>
2023-09-27 18:39:06 +0530

    router issue

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,16 +3,24 @@ import Section01 from './Components/Section01'
 import Section02 from './Components/Section02'
 import Section03 from './Components/Section03'
 import GoToTop from './Components/GoToTop';
+import Footer from './Components/Footer';
+import NavBar from "./Components/NavBar";
+
 
 export default function Home() {
   document.title = 'WorkFolios ';
 
   return (
     <>
-    <Section01/>
-    <Section02/>
-    <Section03/>
-    {/* <GoToTop/> */}
+    <div>
+    <NavBar />
+
+      <Section01/>
+      <Section02/>
+      <Section03/>
+      <GoToTop/>
+    </div>
+    
     
     </>
 
