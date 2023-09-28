4830c5ed6002d99d9b2ac59d1edc7ffb809e08c7
soumen bhunia <sob99338@gmail.com>
2023-09-28 21:33:54 +0530

    router links added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import FormResume from './Components/FormResume';
+import Home from './Home';
+
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element:<App/>,
+
+    children:[
+      {
+        path:'',
+        element:<Home/>
+      },
+      {
+        path:'/WorkFolio',
+        element:<Home/>
+      },
+      {
+        path:'/FormResume',
+        element:<FormResume/>
+      }
+      
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>
 );
+
+
 
 
